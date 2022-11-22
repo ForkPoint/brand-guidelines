3b79d462664d9a24d420262d0dead94b2c738379
Tsvetomir Ivanov <cvetomirkrasimirov96@abv.bg>
2022-11-22 10:58:44 +0200

    Modified typography section, added table with the given typographies

--- src/components/TypographyPage/FontVariants.js
+++ src/components/TypographyPage/FontVariants.js
@@ -4,57 +4,45 @@ import styles from "./FontVariants.module.css";
 const FontVariants = ({}) => {
   return (
     <div className={styles.sectionContainer}>
-      <div className={styles.row}>
-        <div className={styles.variant}>
-          <div className={styles.light}>
-            <p>
+      <table className={styles.typographyTable}>
+        <tbody>
+          <tr>
+            <td className={styles.light}>Open Sans Light</td>
+            <td className={styles.light}>
               abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789
               !@#$%^&*()
-            </p>
-          </div>
-          <h3>Open Sans Light</h3>
-        </div>
-        <div className={styles.variant}>
-          <div className={styles.regular}>
-            <span>
+            </td>
+          </tr>
+          <tr>
+            <td className={styles.regular}>Open Sans Regular</td>
+            <td className={styles.regular}>
               abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789
               !@#$%^&*()
-            </span>
-          </div>
-          <h3>Open Sans Regular</h3>
-        </div>
-      </div>
-      <div className={styles.row}>
-        <div className={styles.variant}>
-          <div className={styles.semibold}>
-            <span>
+            </td>
+          </tr>
+          <tr>
+            <td className={styles.semibold}>Open Sans Semibold</td>
+            <td className={styles.semibold}>
               abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789
               !@#$%^&*()
-            </span>
-          </div>
-          <h3>Open Sans Semibold</h3>
-        </div>
-        <div className={styles.variant}>
-          <div className={styles.bold}>
-            <span>
+            </td>
+          </tr>
+          <tr>
+            <td className={styles.bold}>Open Sans Bold</td>
+            <td className={styles.bold}>
               abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789
               !@#$%^&*()
-            </span>
-          </div>
-          <h3>Open Sans Bold</h3>
-        </div>
-      </div>
-      <div className={styles.row}>
-        <div className={styles.variant}>
-          <div className={styles.extrabold}>
-            <span>
+            </td>
+          </tr>
+          <tr>
+            <td className={styles.extrabold}>Open Sans Extrabold</td>
+            <td className={styles.extrabold}>
               abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789
               !@#$%^&*()
-            </span>
-          </div>
-          <h3>Open Sans Extrabold</h3>
-        </div>
-      </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
